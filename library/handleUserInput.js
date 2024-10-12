e87734cb9b0c3f8eb714ecf66c4cb0d34933b775
whowouldwin <leranetwork@gmail.com>
2024-10-13 00:19:10 +0300

    refactor: refactor if statements to switch-case for cleaner control flow

--- library/handleUserInput.js
+++ library/handleUserInput.js
@@ -1,9 +1,20 @@
 export async function handleUserInput (line, rl, user, currentDir) {
   const input = line.trim();
-  if (input === '.exit') {
-    rl.close();
-  } else {
-    console.log('Invalid input')
+  const args = input.split(' ')
+  const command = args[0];
+
+  try {
+    switch (command) {
+      case '.exit':
+        rl.close();
+        return;
+      default:
+        console.log('Invalid input')
+        break;
+    }
+  } catch (e) {
+    console.log('Operation failed')
   }
+  console.log(`You are currently in ${currentDir}`);
   rl.prompt();
 }
