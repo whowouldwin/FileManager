955159322f19691ac1f130668f2913522a662713
whowouldwin <leranetwork@gmail.com>
2024-10-14 19:49:12 +0300

    refactor: remove duplicate code

--- library/utils/compression/fileCompression.js
+++ library/utils/compression/fileCompression.js
@@ -2,69 +2,50 @@ import * as path from 'node:path';
 import fs from 'fs';
 import * as zlib from 'node:zlib';
 
-export async function compressFile(args, currentDir) {
+async function processFile(args, currentDir, operation) {
   if (args.length < 3) {
     console.log('Please add a source file and destination file');
     return;
   }
+
   const filePath = path.resolve(currentDir, args[1]);
   let destPath = path.resolve(currentDir, args[2]);
 
   if (fs.existsSync(destPath) && fs.statSync(destPath).isDirectory()) {
-    const fileName = path.basename(filePath) + '.br';
+    const fileName = operation === 'compress'
+      ? path.basename(filePath) + '.br'
+      : path.basename(filePath, '.br');
     destPath = path.join(destPath, fileName);
   }
+
   if (!fs.existsSync(filePath)) {
     console.log('Operation failed');
     return;
   }
+
   return await new Promise((resolve, reject) => {
     const inputStream = fs.createReadStream(filePath);
     const outputStream = fs.createWriteStream(destPath);
-    const brotli = zlib.createBrotliCompress();
+
+    const brotli = operation === 'compress' ?
+      zlib.createBrotliCompress() : zlib.createBrotliDecompress();
 
     outputStream.on('error', (err) => {
-      console.log('Operation failed. Error during compression', err);
-      reject(new Error('Error during compression'));
+      console.log(`Operation failed. Error during ${operation}`, err);
+      reject(new Error(`Error during ${operation}`));
     })
     outputStream.on('finish', () => {
-      console.log('Successfully compressed file');
+      console.log(`Successfully ${operation} file`);
       resolve();
     });
     inputStream.pipe(brotli).pipe(outputStream);
   });
 }
 
+export async function compressFile(args, currentDir) {
+  return processFile(args, currentDir, 'compress')
+}
+
 export async function decompressFile(args, currentDir) {
-  if (args.length < 3) {
-    console.log('Please add a source file and destination file');
-    return;
-  }
-
-  const filePath = path.resolve(currentDir, args[1]);
-  let destPath = path.resolve(currentDir, args[2]);
-
-  if (fs.existsSync(destPath) && fs.statSync(destPath).isDirectory()) {
-    const fileName = path.basename(filePath, '.br');
-    destPath = path.join(destPath, fileName);
-  }
-  if (!fs.existsSync(filePath)) {
-    console.log('Operation failed');
-    return;
-  }
-  return await new Promise((resolve, reject) => {
-    const inputStream = fs.createReadStream(filePath);
-    const outputStream = fs.createWriteStream(destPath);
-    const brotli = zlib.createBrotliDecompress()
-
-    outputStream.on('error', (err) => {
-      console.log('Operation failed. Error during compression', err);
-      reject(new Error('Error during compression'));
-    })
-    outputStream.on('finish', () => {
-      console.log('Successfully decompressed file');
-      resolve();
-    });
-    inputStream.pipe(brotli).pipe(outputStream);
-  });
+  return processFile(args, currentDir, 'decompress')
 }
