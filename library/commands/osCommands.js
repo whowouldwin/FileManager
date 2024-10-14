4c85228679a694a782607913f9f4ed002b544ff3
whowouldwin <leranetwork@gmail.com>
2024-10-14 17:14:28 +0300

    feat: os --cpus command

--- library/commands/osCommands.js
+++ library/commands/osCommands.js
@@ -1,4 +1,5 @@
 import * as os from 'node:os';
+import { cpusCommand } from '../utils/cpus/cpusCommand.js';
 
 export async function osCommands(args) {
   const command = args[1]
@@ -10,6 +11,9 @@ export async function osCommands(args) {
     switch (command) {
       case '--EOL':
         console.log(`Default symbol is ${JSON.stringify(os.EOL)}`);
+        break;
+      case '--cpus':
+        await cpusCommand();
         break;
       default:
         console.log('Operation failed. Please use the correct flags!');
