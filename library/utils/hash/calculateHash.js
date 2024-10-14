33b4bb61dcd80545b3b15a45fb514544fe7a7a99
whowouldwin <leranetwork@gmail.com>
2024-10-14 18:29:35 +0300

    refactor: rewrite calculateHash with async/await for cleaner code

--- library/utils/hash/calculateHash.js
+++ library/utils/hash/calculateHash.js
@@ -3,44 +3,39 @@ import fs from 'fs';
 import { createHash } from 'crypto';
 
 export async function calculateHash(args, rl, currentDir) {
-  return new Promise((resolve, reject) => {
-    if (args.length < 2) {
-      console.log('Operation failed. Please add the file path.')
-      rl.prompt();
-      resolve();
-      return;
-    }
-    const filePath = path.isAbsolute(args[1]) ? args[1] : path.join(currentDir, args[1]);
+  if (args.length < 2) {
+    console.log('Operation failed. Please add the file path.')
+    rl.prompt();
+    return;
+  }
+  const filePath = path.isAbsolute(args[1]) ? args[1] : path.join(currentDir, args[1]);
 
-    try {
-      if (!fs.existsSync(filePath)) {
-        console.log('Operation failed. File does not exist');
-        rl.prompt();
-        resolve();
-        return;
-      }
+  if (!fs.existsSync(filePath)) {
+    console.log('Operation failed. File does not exist');
+    rl.prompt();
+    return;
+  }
+  try {
+    const hash = createHash('sha256');
+    const stream = fs.createReadStream(filePath);
 
-      const hash = createHash('sha256');
-      const stream = fs.createReadStream(filePath);
-
+    await new Promise((resolve, reject) => {
       stream.on('data', (chunk) => {
         hash.update(chunk);
       });
       stream.on('end', () => {
-        const fileHash = hash.digest('hex');
-        console.log(`Hash of the file: ${fileHash}`);
-        rl.prompt();
         resolve();
       })
       stream.on('error', (e) => {
-        console.log('Operation failed. Could not read file');
-        rl.prompt();
         reject(e);
       })
-    } catch (e) {
-      console.log(`Operation failed. Error: ${e.message}`);
-      rl.prompt();
-      reject(e);
-    }
-  })
+    })
+
+    const fileHash = hash.digest('hex');
+    console.log(`Hash of the file: ${fileHash}`);
+
+  } catch (e) {
+    console.log(`Operation failed. Error: ${e.message}`);
+  }
+  rl.prompt();
 }
